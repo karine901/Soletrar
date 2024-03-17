4a3f6381e7fe097b55e5ee965eda540928adb5e6
Meu karine <meu-km123silva2339@meu-karine901.com>
2024-03-17 14:37:33 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+Console.Clear();
+
+Console.WriteLine("soletrando\n");
+Console.Write("digite o texto a ser soletrando:");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nsoletrando ficaria...\n");
+
+Console.WriteLine(string.Join("-",texto.ToUpper().ToCharArray()));
+
+Console.WriteLine();
